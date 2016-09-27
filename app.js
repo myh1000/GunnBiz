581ffcb06b2ee3633bf05e7f11ff0bab1c519663
myh1000 <myh1000@gmail.com>
2016-09-27 10:48:53 -0700

    good stuff

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ mongoose.connect(dbConfig.url);
 app.set('views', 'views');
 app.set('view engine', 'ejs');
 
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
