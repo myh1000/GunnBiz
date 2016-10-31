1758aef8488829f7e2ac89e6deab8a00ccf5584f
myh1000 <myh1000@gmail.com>
2016-10-31 14:29:09 -0700

    merge

---
+++ routes/index.js
@@ -0,0 +1,234 @@
+var User = require('../passport/profile');
+var async = require('async');
+var crypto = require('crypto');
+var bCrypt = require('bcrypt-nodejs');
+
+module.exports = function(app, passport){
+
+	var mongoose = require('mongoose')
+	app.use(function (req, res, next) {
+		res.locals.login = req.isAuthenticated();
+		next();
+	});
+
+    /* GET home page. */
+    app.get('/', function(req, res, next) {
+      res.render('home', { title: 'Gunn Business - Home' });
+    });
+	app.get('/news', function(req, res, next) {
+      res.render('news', { title: 'Gunn Business - News' });
+    });
+	app.get('/projects', function(req, res, next) {
+      res.render('projects', { title: 'Gunn Business - Projects' });
+    });
+	app.get('/ae', function(req, res, next) {
+      res.render('ae', { title: 'Gunn Business - American Enterprise' });
+    });
+	app.get('/pb', function(req, res, next) {
+      res.render('pb', { title: 'Gunn Business - Partnership with Business' });
+    });
+	app.get('/cs', function(req, res, next) {
+      res.render('cs', { title: 'Gunn Business - Community Service' });
+    });
+    /* GET About page. */
+    app.get('/about', function(req, res, next) {
+      res.render('about', { title: 'Gunn Business - About' });
+    });
+    /* GET index page. */
+    app.get('/index', function(req, res, next) {
+      res.render('home', { title: 'Gunn Business - Home' });
+    });
+    /* GET events page. */
+    app.get('/events', function(req, res, next) {
+      res.render('events', { title: 'Gunn Business - Events' });
+    });
+	/* GET New User page. */
+	app.get('/login', function(req, res) {
+		if(req.isAuthenticated(req, res)) {
+            res.redirect('/profile');
+        } else {
+			res.render('login', { title: 'Gunn Business - Login', message: req.flash('message')});
+		}
+	});
+	app.post('/login', passport.authenticate('login', {
+		successRedirect: '/profile',
+		failureRedirect: '/login',
+		failureFlash : true
+	}));
+	/* GET New User page. */
+	app.get('/registration', function(req, res) {
+		if(req.isAuthenticated(req, res)) {
+            res.redirect('/profile');
+        } else {
+			res.render('registration', { title: 'Gunn Business - User Registration', message: req.flash('message')});
+		}
+	});
+    /* Handle Registration POST */
+    app.post('/registration', passport.authenticate('signup', {
+		successRedirect: '/profile',
+		failureRedirect: '/registration',
+		failureFlash : true
+	}));
+	app.get('/logout', function(req, res){
+		req.logout();
+		res.redirect('/');
+	});
+	// app.get('/forgot', function(req, res) {
+	// 	if(req.isAuthenticated(req, res)) {
+    //         res.redirect('/profile');
+    //     } else {
+	// 		res.render('forgot', {
+	// 			user: req.user,
+	// 			title: 'Gunn Business - Forgot Password',
+	// 			message: req.flash('message')
+	// 		});
+	// 	}
+	// });
+	// app.post('/forgot', function(req, res, next) {
+	// 	async.waterfall([
+	// 	    function(done) {
+	// 	      crypto.randomBytes(20, function(err, buf) {
+	// 	        var token = buf.toString('hex');
+	// 	        done(err, token);
+	// 	      });
+	// 	    },
+	// 	    function(token, done) {
+	// 	      User.findOne({ email: req.body.email }, function(err, user) {
+	// 	        if (!user) {
+	// 	          req.flash('error', 'No account with that email address exists.');
+	// 	          return res.redirect('/forgot');
+	// 	        }
+	//
+	// 	        user.resetPasswordToken = token;
+	// 	        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+	// 			console.log(token);
+	// 	        user.save(function(err) {
+	// 	          done(err, token, user);
+	// 	        });
+	// 	      });
+	// 	    }
+	// 	    // function(token, user, done) {
+	// 		// 	var smtpTransport = nodemailer.createTransport('SMTP', {
+	// 		// 			service: 'SendGrid',
+	// 		// 			auth: {
+	// 		// 			user: '',
+	// 		// 			pass: ''
+	// 		// 		}
+	// 		// 	});
+	// 		// 	var mailOptions = {
+	// 		// 		to: user.email,
+	// 		// 		from: 'passwordreset@demo.com',
+	// 		// 		subject: 'Node.js Password Reset',
+	// 		// 		text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+	// 		// 		'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+	// 		// 		'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+	// 		// 		'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+	// 		// 	};
+	// 		// 	smtpTransport.sendMail(mailOptions, function(err) {
+	// 		// 		req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+	// 		// 		done(err, 'done');
+	// 		// 	});
+	// 		// }
+	// 	], function(err) {
+	// 		if (err) return next(err);
+	// 		res.redirect('/forgot');
+	// 	});
+	// });
+	app.get('/reset/:token', function(req, res) {
+		User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+			if (!user) {
+				req.flash('error', 'Password reset token is invalid or has expired.');
+				return res.redirect('/forgot');
+			}
+			res.render('reset', {
+				user: req.user,
+				title: 'Gunn Business - Reset Password'
+			});
+		});
+	});
+	app.post('/reset/:token', function(req, res) {
+		async.waterfall([
+			function(done) {
+				User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+				if (!user) {
+				  req.flash('error', 'Password reset token is invalid or has expired.');
+				  return res.redirect('back');
+				}
+
+				user.password = bCrypt.hashSync(req.body.password, bCrypt.genSaltSync(10), null);
+				user.resetPasswordToken = undefined;
+				user.resetPasswordExpires = undefined;
+
+				user.save(function(err) {
+					req.logIn(user, function(err) {
+					done(err, user);
+					});
+				});
+			});
+		}
+		], function(err) {
+		res.redirect('/');
+		});
+	});
+	/* GET Profile Page */
+	app.get('/profile', isAuthenticated, function(req, res){
+		res.render('profile', { title: 'Gunn Business - Profile', user: req.user, message: req.flash('message')});
+	});
+	app.post('/setinfo', function(req, res) {
+		req.user.firstName = req.body.firstName;
+		req.user.lastName = req.body.lastName;
+		req.user.grade = req.body.grade;
+		req.user.birthday = req.body.birthday;
+		req.user.studentID = req.body.studentID;
+		req.user.parent1email = req.body.parent1email;
+		req.user.parent2email = req.body.parent2email;
+		req.user.save(function (err, member) {
+			if (err) return console.error(err);
+		});
+		res.redirect('/profile');
+	});
+	app.post('/setDECARegionals', function(req, res)
+	{
+		console.log(req.body.DECA_regionalsWritten);
+		req.user.DECA_regionalsWritten = req.body.DECA_regionalsWritten;
+		req.user.DECA_writtenPartner1 = req.body.DECA_writtenPartner1;
+		req.user.DECA_writtenPartner2 = req.body.DECA_writtenPartner2;
+		req.user.DECA_regionalsRoleplay = req.body.DECA_regionalsRoleplay;
+		req.user.DECA_roleplayPartner = req.body.DECA_roleplayPartner;
+		// req.user.DECA_regionalsTShirt = req.body.DECA_regionalsTShirt;
+		req.user.DECA_regionalsRegistered = 1;
+		req.user.save(function (err, member) {
+			if (err) return console.error(err);
+			console.log("saved");
+		});
+		res.redirect('/profile');
+	});
+	app.post('/setFBLARegionals', function(req, res)
+	{
+		console.log(req.body.DECA_regionalsWritten);
+		req.user.FBLA_regionalsWritten = req.body.FBLA_regionalsWritten;
+		req.user.FBLA_writtenPartner1 = req.body.FBLA_writtenPartner1;
+		req.user.FBLA_writtenPartner2 = req.body.FBLA_writtenPartner2;
+		req.user.FBLA_regionalsRoleplay = req.body.FBLA_regionalsRoleplay;
+		req.user.FBLA_roleplayPartner = req.body.FBLA_roleplayPartner;
+		// req.user.FBLA_regionalsTShirt = req.body.FBLA_regionalsTShirt;
+		req.user.FBLA_regionalsRegistered = 1;
+		req.user.save(function (err, member) {
+			if (err) return console.error(err);
+			console.log("saved");
+		});
+		res.redirect('/profile');
+	});
+
+    // return app;
+}
+
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
