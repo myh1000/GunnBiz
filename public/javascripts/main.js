51408d7327a217a895625c0acf2f0530539832c6
myh1000 <myh1000@gmail.com>
2016-11-13 14:52:12 -0800

    change carousel speed

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,8 @@
 // Sets height of blue-three columns to be equal
-var h = document.querySelector(".blue-three").offsetHeight;
-document.querySelector(".blue-three div:nth-child(1)").style.height = h + "px";
-document.querySelector(".blue-three div:nth-child(2)").style.height = h + "px";
-document.querySelector(".blue-three div:nth-child(3)").style.height = h + "px";
+// var h = document.querySelector(".blue-three").offsetHeight;
+// document.querySelector(".blue-three div:nth-child(1)").style.height = h + "px";
+// document.querySelector(".blue-three div:nth-child(2)").style.height = h + "px";
+// document.querySelector(".blue-three div:nth-child(3)").style.height = h + "px";
 // Activates Carousel
 $('.carousel').carousel({
     interval: 2000 //changes the speed
